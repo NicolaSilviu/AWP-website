8b7398c9aebfdc1c544a417227dcf36413a04d11
Silviu Nicola <silviu.work1@gmail.com>
2022-11-24 22:42:40 +0200

    implemented active sections with different colors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 
 function PageTransitions(){
@@ -13,6 +13,26 @@ function PageTransitions(){
             this.className += ' active-btn'
         })
     }
+
+    //Sections Active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            // Remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // Hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
